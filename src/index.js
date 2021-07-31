9d741f0c2cbba0b92da9e21502756db97713b9ed
Dmitriy Grigoryev <dmitriygrigoryev@Dmitriys-MacBook-Air.local>
2021-07-31 17:15:46 +0300

    step#1.1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
 import 'bootstrap';
 import './scss/styles.scss';
+import onChange from 'on-change';
+
+const engine = () => {
+  const state = {
+    queryForm: {
+      state: 'valid',
+      data: [],
+    },
+    errors: [],
+  };
+  const watchedState = onChange(state, (path, value) => {
+    if (path === 'queryForm.data') {
+      console.log(value);
+    }
+    if (path === 'queryForm.state') {
+      switch (value) {
+        case 'valid':
+          console.log(value);
+          break;
+        case 'filling':
+          console.log(value);
+          break;
+        default:
+          throw new Error('Unknown queryForm state');
+      }
+    }
+  });
+  const form = document.querySelector('form');
+  const queryField = form.elements.url;
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    watchedState.queryForm.data.push(form.elements.url.value);
+  });
+  queryField.addEventListener('input', () => {
+    watchedState.queryForm.state = 'filling';
+  });
+};
+engine();
