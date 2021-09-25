e180ffce7f5b6c281d5ea94452d5d824e920eb5f
Dmitriy Grigoryev <dmitriygrigoryev@Dmitriys-MacBook-Air.local>
2021-09-26 00:02:37 +0300

    step#6 disable UI

--- src/init.js
+++ src/init.js
@@ -11,25 +11,6 @@ import parseRSS from './parseRSS.js';
 import update from './update.js';
 
 const init = () => {
-  i18next.init({
-    lng: 'ru',
-    resources,
-  });
-
-  setLocale({
-    mixed: {
-      required: i18next.t('errors.emptyField'),
-    },
-    string: {
-      url: i18next.t('errors.notValidURL'),
-    },
-  });
-
-  const schema = yup.string().required().url();
-
-  const form = document.querySelector('.rss-form');
-  const input = document.querySelector('input[name=url]');
-
   const state = {
     form: {
       state: '',
@@ -49,72 +30,93 @@ const init = () => {
     },
   };
 
-  const watchedState = onChange(state, (...params) => {
-    watcher(watchedState, ...params);
-  });
-
-  update(watchedState);
-
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    schema
-      .validate(input.value)
-      .then((value) => {
-        watchedState.form.state = 'init';
-        if (watchedState.data.links.findIndex((l) => l === value) !== -1) {
-          watchedState.form.error = 'alreadyExist';
-          watchedState.form.state = 'invalid';
-        } else {
-          watchedState.data.links.push(value);
-          watchedState.form.state = 'valid';
-          watchedState.form.error = null;
-        }
-        return value;
-      })
-      .then((value) => {
-        if (watchedState.form.state === 'valid') {
-          watchedState.network.state = 'loading';
-          axios
-            .get(
-              `https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${encodeURIComponent(
-                value
-              )}`
-            )
-            .then((response) => {
-              const parsedData = parseRSS(response.data.contents);
-              if (parsedData) {
-                let counter = watchedState.data.posts.length + 1;
-                const parsedPostsIdAdded = parsedData.posts.map((post) => {
-                  const result = post;
-                  result.id = counter;
-                  counter += 1;
-                  return result;
-                });
-                watchedState.data.feedCount += 1;
-                const postsArray = [
-                  ...watchedState.data.posts,
-                  ...parsedPostsIdAdded,
-                ];
-                watchedState.data.posts = postsArray;
-                watchedState.data.feed = parsedData.feed;
-                watchedState.network.state = 'success';
-                watchedState.network.state = 'init';
-              } else {
-                watchedState.network.state = 'parserError';
-              }
-            })
-            .catch((er) => {
-              console.log('network error', er);
-              watchedState.network.state = 'failed';
-            });
-        }
-      })
-      .catch((err) => {
-        watchedState.form.state = 'init';
-        watchedState.form.error = err.errors;
-        console.log('error', watchedState.form.error);
-        watchedState.form.state = 'invalid';
+  const i18nextInstance = i18next.createInstance();
+  return i18nextInstance
+    .init({
+      lng: 'ru',
+      resources,
+    })
+    .then((i18n) => {
+      setLocale({
+        mixed: {
+          required: i18n('errors.emptyField'),
+        },
+        string: {
+          url: i18n('errors.notValidURL'),
+        },
       });
-  });
+      const schema = yup.string().required().url();
+
+      const form = document.querySelector('.rss-form');
+      const input = document.querySelector('input[name=url]');
+
+      const watchedState = onChange(state, (...params) => {
+        watcher(watchedState, i18nextInstance, ...params);
+      });
+
+      update(watchedState);
+
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        schema
+          .validate(input.value)
+          .then((value) => {
+            watchedState.form.state = 'init';
+            if (watchedState.data.links.findIndex((l) => l === value) !== -1) {
+              watchedState.form.error = 'alreadyExist';
+              watchedState.form.state = 'invalid';
+            } else {
+              watchedState.data.links.push(value);
+              watchedState.form.state = 'valid';
+              watchedState.form.error = null;
+            }
+            return value;
+          })
+          .then((value) => {
+            if (watchedState.form.state === 'valid') {
+              watchedState.network.state = 'loading';
+              axios
+                .get(
+                  `https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${encodeURIComponent(
+                    value
+                  )}`
+                )
+                .then((response) => {
+                  const parsedData = parseRSS(response.data.contents);
+                  if (parsedData) {
+                    let counter = watchedState.data.posts.length + 1;
+                    const parsedPostsIdAdded = parsedData.posts.map((post) => {
+                      const result = post;
+                      result.id = counter;
+                      counter += 1;
+                      return result;
+                    });
+                    watchedState.data.feedCount += 1;
+                    const postsArray = [
+                      ...watchedState.data.posts,
+                      ...parsedPostsIdAdded,
+                    ];
+                    watchedState.data.posts = postsArray;
+                    watchedState.data.feed = parsedData.feed;
+                    watchedState.network.state = 'success';
+                    watchedState.network.state = 'init';
+                  } else {
+                    watchedState.network.state = 'parserError';
+                  }
+                })
+                .catch((er) => {
+                  console.log('network error', er);
+                  watchedState.network.state = 'failed';
+                });
+            }
+          })
+          .catch((err) => {
+            watchedState.form.state = 'init';
+            watchedState.form.error = err.errors;
+            console.log('error', watchedState.form.error);
+            watchedState.form.state = 'invalid';
+          });
+      });
+    });
 };
 export default init;
--- src/view.js
+++ src/view.js
@@ -1,12 +1,13 @@
-import i18next from 'i18next';
+// import i18next from 'i18next';
 import renderPosts from './renderPosts';
 import renderFeed from './renderFeed';
 import renderModal from './renderModal';
 
-export default (state, ...params) => {
+export default (state, i18next, ...params) => {
   const form = document.querySelector('form');
   const feedback = document.querySelector('p.feedback');
   const input = document.querySelector('input[name=url]');
+  const button = document.querySelector('button[type="submit"]');
 
   const handleFormState = (value) => {
     switch (value) {
@@ -34,19 +35,27 @@ export default (state, ...params) => {
     switch (value) {
       case 'loading':
         feedback.textContent = '';
+        input.setAttribute('readonly', 'readonly');
+        button.disabled = true;
         break;
       case 'success':
+        input.removeAttribute('readonly');
+        button.disabled = false;
         feedback.classList.remove('text-danger');
         feedback.classList.add('text-success');
         feedback.textContent = i18next.t('loaded');
         break;
       case 'failed':
+        input.removeAttribute('readonly');
+        button.disabled = false;
         feedback.classList.remove('text-success');
         feedback.classList.add('text-danger');
         feedback.textContent = i18next.t('errors.loadError');
         break;
       case 'parserError':
+        input.removeAttribute('readonly');
         feedback.classList.remove('text-success');
+        button.disabled = false;
         feedback.classList.add('text-danger');
         feedback.textContent = i18next.t('errors.parserError');
         break;
