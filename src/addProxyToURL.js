f0caedfb5d957f34f0181ffc52123f60e49a547d
Dmitriy Grigoryev <dmitriygrigoryev@Dmitriys-MacBook-Air.local>
2021-11-06 18:52:26 +0300

    step#7 searchParams fix

--- src/addProxyToURL.js
+++ src/addProxyToURL.js
@@ -1,6 +1,6 @@
 export default (url) => {
-  const urlProxyAdded = new URL('https://hexlet-allorigins.herokuapp.com/get?');
-  urlProxyAdded.searchParams.set('disableCache', 'true');
+  const urlProxyAdded = new URL('get', 'https://hexlet-allorigins.herokuapp.com');
+  urlProxyAdded.searchParams.set('disableCache', true);
   urlProxyAdded.searchParams.set('url', url);
-  return urlProxyAdded;
+  return urlProxyAdded.toString();
 };
