7ae7f9642e751b6d2443fdb82ccbac74293d3fa9
Dmitriy Grigoryev <dmitriygrigoryev@Dmitriys-MacBook-Air.local>
2023-04-05 18:26:22 +0300

    step#7 proxying requests improved

--- src/addProxyToURL.js
+++ src/addProxyToURL.js
@@ -1,6 +1,1 @@
-export default (url) => {
-  const urlProxyAdded = new URL('get', 'https://hexlet-allorigins.herokuapp.com');
-  urlProxyAdded.searchParams.set('disableCache', true);
-  urlProxyAdded.searchParams.set('url', url);
-  return urlProxyAdded.toString();
-};
+export default (url) => `https://allorigins.hexlet.app/get?disableCache=true&url=${url}`;
